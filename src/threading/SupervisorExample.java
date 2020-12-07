d89691a7300ced45a37e931ca5a9e9011a232958
sabri baazaoui <baazaouisabri@gmail.com>
2020-12-07 20:29:43 +0100

    threading

--- src/threading/SupervisorExample.java
+++ src/threading/SupervisorExample.java
@@ -1,30 +1,40 @@
 package threading;
 
+public class SupervisorExample {
+    public static void main(String[] args){
+        Worker1 worker1 = new Worker1();
+        Worker2 worker2 = new Worker2();
+        try {
+            worker1.executeWork();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-  public class SupervisorExample {
+        worker2.executeWork();
 
- public static void main(String[] args) {
-     Worker1 worker1 = new Worker1();
-  Worker2 worker2 = new Worker2();
-  worker1.executeWork();
-  worker2.executeWork();
 
-  }
+    }
+}
 
- }
+class Worker1 {
+    public void executeWork() throws InterruptedException {
+        for (int i=0;i<10;i++){
+            Thread.sleep(100);
+            System.out.println("Worker 1 is executing task : " + i);
+        }
+    }
+}
 
- class Worker1{
- public void executeWork() { for (int i=0;i<10;i++)
-  {
- System.out.println("Worker 1 is executing task#" + i);
-  }
- }
-  }
+class Worker2 {
+    public void executeWork() {
+        for (int i=0;i<10;i++){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Worker 2 is executing task : " + i);
+        }
+    }
 
- class Worker2{
- public void executeWork() {
-     for (int i=0;i<10;i++) {
- System.out.println ("Worker 2 is executing task#" + i);
-     }
- }
-  }
+}
